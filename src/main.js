1948c12912663d8c57efde94ca75accf825859bf
ElenaKorsakova <lenawwka@gmail.com>
2022-08-14 14:01:42 +0200

    Создаст презентер маршрута

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import FiltersView from './view/filters-view';
+import SortView from './view/sort-views';
+import RoadPresenter from './presenter/presenter-road';
 import { render } from './render';
 
-const siteHeaderElement = document.querySelector('.page-header');
+const siteFiltersElement = document.querySelector('.trip-controls__filters');
+const siteRoadContainerElement = document.querySelector('.trip-events');
+const roadPresenter = new RoadPresenter();
 
-render(new FiltersView, siteHeaderElement);
+render(new FiltersView(), siteFiltersElement);
+render(new SortView(), siteRoadContainerElement,'afterbegin');
+
+roadPresenter.init(siteRoadContainerElement);
