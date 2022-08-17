576cab7f5cae68763f1c0350bbaf27bd3fb8438b
ElenaKorsakova <lenawwka@gmail.com>
2022-08-17 14:56:04 +0200

    Добавит PointView методы для подстановки данных в разметку

--- src/view/point-view.js
+++ src/view/point-view.js
@@ -12,6 +12,61 @@ export default class PointView extends BaseView {
     return createAdjacentHtml();
   }
 
+  /**
+   * Установит дату события
+   * @param {string}
+   * @returns
+   */
+  setDate(date) {
+    this.querySelector('.event__date').textContent = date;
+
+    return this;
+  }
+
+  /**
+   * Установит название события
+   * @param {string}
+   * @returns
+   */
+  setTitle(title) {
+    this.querySelector('.event__title').textContent = title;
+
+    return this;
+  }
+
+  /**
+   * Установит время начала события
+   * @param {string}
+   * @returns
+   */
+  setTimeFrom(time) {
+    this.querySelector('.event__start-time').textContent = time;
+
+    return this;
+  }
+
+  /**
+     * Установит время окончания события
+     * @param {string}
+     * @returns
+     */
+  setTimeTo(time) {
+    this.querySelector('.event__end-time').textContent = time;
+
+    return this;
+  }
+
+  /**
+     * Установит цену события
+     * @param {number}
+     * @returns
+     */
+  setPrice(price) {
+    this.querySelector('.event__price-value').textContent = price;
+
+    return this;
+  }
+
   addOffer(...views) {
     this.querySelector('.event__selected-offers').append(...views);
 
