ab32363c9e006bd279b1a2a8f871d77ef92ebd0b
ElenaKorsakova <lenawwka@gmail.com>
2022-08-17 14:49:06 +0200

    Заведет директорию и создаст структуру с моковыми данными

---
+++ src/mock/mock-point.js
@@ -0,0 +1,29 @@
+
+const generatePoint = () => ({
+  basePrice: 1100,
+  dateFrom: '2019-07-10T22:55:56.845Z',
+  dateTo: '2019-07-11T11:22:13.375Z',
+  destination: {
+    id: 1,
+    description: 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Chamonix',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163317',
+        description: 'Chamonix parliament building'
+      }
+    ]
+  },
+  id: 0,
+  offers: [
+    {
+      id: 1,
+      title: 'Upgrade to a business class',
+      price: 120
+    }
+  ],
+  type: 'bus'
+}
+);
+
+export {generatePoint};
