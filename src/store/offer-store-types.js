34491988c38c599e43734fcc616223b673f9a05b
ElenaKorsakova <lenawwka@gmail.com>
2022-08-18 11:57:33 +0200

    Добавит файл конфигурации и структуры данных с сервера

---
+++ src/store/offer-store-types.js
@@ -0,0 +1,16 @@
+/**
+ * @typedef OfferGroup
+ * @prop {OfferType} type
+ * @prop {Offer[]} offers
+ */
+
+/**
+ * @typedef {'taxi' | 'bus' | 'train' | 'ship' | 'drive' | 'flight' | 'check-in' | 'sightseeing' | 'restaurant'} OfferType
+ */
+
+/**
+ * @typedef Offer
+ * @prop {number} id
+ * @prop {string} title
+ * @prop {number} price
+ */
---
+++ src/store/point-store-types.js
@@ -0,0 +1,23 @@
+/**
+ * @typedef Point
+ * @prop {number} base_price
+ * @prop {string} date_from
+ * @prop {string} date_to
+ * @prop {number} destination
+ * @prop {string} id
+ * @prop {boolean} is_favorite
+ * @prop {number[]} offers
+ * @prop {OfferType} type
+ */
+
+
+/**
+ * @typedef LocalPoint
+ * @prop {number} base_price
+ * @prop {string} date_from
+ * @prop {string} date_to
+ * @prop {number} destination
+ * @prop {boolean} is_favorite
+ * @prop {number[]} offers
+ * @prop {OfferType} type
+ */
