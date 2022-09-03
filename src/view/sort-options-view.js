7c04a1fb32e9a8b47315df332e69580893d5a25a
ElenaKorsakova <lenawwka@gmail.com>
2022-09-03 14:05:12 +0200

    feat: реализует сортировку

--- src/view/sort-options-view.js
+++ src/view/sort-options-view.js
@@ -1,32 +1,36 @@
 import ComponentView, {html} from './component-view.js';
+/** @typedef {[label: string, value: string]} SortOptionState  */
 
 export default class SortOptionView extends ComponentView {
-  constructor() {
-    super(...arguments);
+  /**
+   * @param {SortOptionState} state
+   */
+  constructor(label, value) {
+    super(label, value);
 
-    this.style.display = 'block';
+    this.classList.add('trip-sort__item', `trip-sort__item--${value}`);
   }
 
   /**
-   * @override
+   *  * @param {OfferState} state
+   *  @override
    */
-  createAdjacentHtml(label, value) {
+  createAdjacentHtml(...state) {
+    const [label, value] = state;
     return html`
-    <div class ="trip-sort__item trip-sort__item--${value}">
-    <input
-     id="sort-${value}"
-      class="trip-sort__input  visually-hidden"
-      type="radio"
-      name="trip-sort"
-      value="sort-${value}"
-    >
-    <label
-      class="trip-sort__btn"
-      for="sort-${value}"
-    >
-      ${label}
-    </label>
-  </div>
+      <input
+        id="sort-${value}"
+        class="trip-sort__input  visually-hidden"
+        type="radio"
+        name="trip-sort"
+        value="${value}"
+      >
+      <label
+        class="trip-sort__btn"
+        for="sort-${value}"
+      >
+        ${label}
+      </label>
     `;
   }
 }
